9912b5f2e7d1ef854f6a41f0f78f8c4199e8ba30
vax-r <richard120310@gmail.com>
2023-08-04 16:19:24 +0800

    Remove duplicate write to StatusOK to prevent superfluous response

--- handlers/healthy.go
+++ handlers/healthy.go
@@ -7,6 +7,5 @@ import (
 
 func HealthyHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "This is a healthy handler!")
-	w.WriteHeader(http.StatusOK)
 	fmt.Fprintln(w, "Status 200 - OK")
 }
